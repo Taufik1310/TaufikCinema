9f619aaa72e1476541079808725be9a4bf0bcb02
Taufik1310 <taufikupik1310@gmail.com>
2023-04-19 05:47:08 +0700

    melakukan strict mode

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot, hydrateRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const StrictApp = () => (
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
+)
+
+const rootElement = document.getElementById('root')
+
+if (rootElement.hasChildNodes()) {
+  hydrateRoot(rootElement, <StrictApp />)
+} else {
+  const root = createRoot(rootElement)
+  root.render(<StrictApp />)
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
